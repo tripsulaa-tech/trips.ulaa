2bf6ded9552367f2e53578a2dc9835c4675fc224
Kabelesh K <trips.ulaa@gmail.com>
2026-08-04 19:10:35 +0530

    bundle

--- vite.config.ts
+++ vite.config.ts
@@ -14,4 +14,18 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+      mangle: true,
+      format: {
+        comments: false,
+      },
+    },
+  },
 })
